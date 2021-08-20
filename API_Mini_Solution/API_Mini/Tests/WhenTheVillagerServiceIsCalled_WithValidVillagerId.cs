e1fac393285232215ee528b6fd5b7583359e4c55
Tak Li <takwing.li98@gmail.com>
2021-08-20 16:01:07 +0100

    Added tests for invalid villager id

---
+++ API_Mini_Solution/API_Mini/Tests/WhenTheVillagerServiceIsCalled_WithValidVillagerId.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using API_Mini.AnimalCrossingIOService;
+using System.Threading.Tasks;
+
+
+namespace API_Mini.Tests
+{
+    public class WhenTheVillagerServiceIsCalled_WithValidVillagerId
+    {
+        VillagerService _villagerService;
+        [OneTimeSetUp]
+        public async Task OneTimeSetupAsync()
+        {
+            _villagerService = new VillagerService();
+            await _villagerService.MakeRequestAsync(64);
+        }
+
+        [Test]
+        public void StatusIs200()
+        {
+            Assert.That(_villagerService.CallManager.StatusCode, Is.EqualTo(200));
+        }
+
+        
+    }
+}
